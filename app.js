0e3c0cb977288aceda94ee30d6172dac5e5cbb26
Varun Kumar <varun13169@users.noreply.github.com>
2022-01-11 01:12:11 +0530

    Update app.js
    Convert to ES6

--- app.js
+++ app.js
@@ -1,37 +1,33 @@
-var btnSubmit = document.querySelector("#btn-submit");
-var txtInput = document.querySelector("#txt-input");
-var txtOutput = document.querySelector("#output-txt");
-var baseUrl = "https://api.funtranslations.com/translate/yoda.json?text="
+const btnSubmit = document.querySelector("#btn-submit");
+const txtInput = document.querySelector("#txt-input");
+const txtOutput = document.querySelector("#output-txt");
+const baseUrl = "https://api.funtranslations.com/translate/yoda.json?text="
 
-txtInput.addEventListener("change", onChangeHandler);
-btnSubmit.addEventListener("click", onClickHandler)
 
-function getTranslation(inpText) {
-    var url = baseUrl + inpText;
+const getTranslation = inpText => {
+    const url = baseUrl + inpText;
 
     return fetch(url)
-        .then((res) => {
-            return res.json()
-        })
-        .then((json) => {
-            return json;
-        });
+        .then(res => res.json())
+        .then(json => json);
 }
 
-function onChangeHandler(e) {
+const onChangeHandler = e => {
     //console.log(e.target.value);
 }
 
-function onClickHandler() {
+const onClickHandler = () => {
 
     getTranslation(txtInput.value)
-        .then((res) => {
-            txtOutput.innerText = res.contents.translated;
-        })
-        .catch((err) => {
+        .then(res => txtOutput.innerText = res.contents.translated)
+        .catch(err => {
             console.log(err);
             alert("Something wrong with server, try again after sometime!");
         });
 }
+
+
+txtInput.addEventListener("change", onChangeHandler);
+btnSubmit.addEventListener("click", onClickHandler);
 
 // "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text="
